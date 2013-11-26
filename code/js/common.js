aa88008ce8059dd0e78f1acb44640db3b0b535e5
aska <aska@aska.xom>
2013-11-27 00:46:49 +0400

    страница - задать вопрос

--- code/js/common.js
+++ code/js/common.js
@@ -17,10 +17,42 @@ $(function() {
 
 
 	    $('.accordion-arrow').click(function() {
-		$(this).parents('.accordion').find('.standard-element__content').slideToggle();
+		$(this).parents('.accordion').find('.standard-element__content').toggleClass('standard-element__content_visible');
 		$(this).find('.ico').toggleClass('ico_content_arrow-up');
+		 StartCarousel();
 	    });
 
+	    StartCarousel();
 
 });
+
+function StartCarousel() {
+    $('.gallery-carousel').each(function(){
+	var galleryWidth = $(this).width();
+	var galleryItemsCount = $(this).find('.thumbnails__item').size() * $(this).find('.thumbnails__item').width();
+
+	if ( galleryWidth < galleryItemsCount) {
+	    if ($(this).children('.next-advice').size() < 1 ) {
+		$(this).append('<span class="next-advice"><i class="ico ico_content_arrow-next"></i></span>');
+	    }
+
+
+	    $(this).find('.thumbnails').carouFredSel({
+		items: 'auto',
+		auto : false,
+		next : ".next-advice",
+
+		items   : {
+		    height:50,
+		    width:50,
+		},
+
+		scroll: {
+		    items: 1,
+		    duration:500
+		}
+	    });
+	}
+    });
+}
 
