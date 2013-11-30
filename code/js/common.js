287e076c48c860f1972b6047f77d678f7418a1b3
aska <aska@aska.xom>
2013-11-30 21:35:55 +0400

    раздел гайды

--- code/js/common.js
+++ code/js/common.js
@@ -2,27 +2,41 @@ $(function() {
 
 // Tabs
 
-	$('.tabs-title').on('click', '.tabs-title__item:not(.tabs-title__item_current)', function() {
-		$(this).addClass('tabs-title__item_current').siblings().removeClass('tabs-title__item_current')
-			.parents('.tabs').find('.tab-content').eq($(this).index()).fadeIn(150).siblings('.tab-content').hide();
+
+$( "#tabs" ).tabs();
+
+$( "#tabs-pseudo" ).tabs( {
+    collapsible: true,
+    active: false
+});
+
+
+
+// Tabs-pseudo
+
+
+
+// Accordion
+
+    $('.accordion-arrow').click(function() {
+	$(this).parents('.accordion').find('.standard-element__content').toggleClass('standard-element__content_visible');
+	$(this).find('.ico').toggleClass('ico_content_arrow-up');
+	StartCarousel();
+    });
+
+    StartCarousel();
+
+
+// Likes
+
+    $('.like-box').each(function() {
+	$(this).click(function() {
+		$(this).toggleClass('like-box_ready');
 	});
 
-	var tabIndex = window.location.hash.replace('#tab','')-1;
-	if (tabIndex != -1) $('.tabs-title .tabs-title__item').eq(tabIndex).click();
-
-	$('.tabs-title__item a[href*=#tab]').click(function() {
-		var tabIndex = $(this).attr('href').replace(/(.*)#tab/, '')-1;
-		$('.tabs-title__item .tabs-title__item').eq(tabIndex).click();
-	});
+    });
 
 
-	    $('.accordion-arrow').click(function() {
-		$(this).parents('.accordion').find('.standard-element__content').toggleClass('standard-element__content_visible');
-		$(this).find('.ico').toggleClass('ico_content_arrow-up');
-		 StartCarousel();
-	    });
-
-	    StartCarousel();
 
 });
 
